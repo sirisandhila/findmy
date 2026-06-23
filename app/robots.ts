ee2507274f8a8b398bc832a68debb4cd1d494fba
SiriSandhila <sirigoud05@gmail.com>
2026-06-23 10:34:10 +0530

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap:
+      "https://findmy-v8cc.vercel.app/sitemap.xml",
+  }
+}
